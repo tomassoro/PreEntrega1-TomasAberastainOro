c08e75bbe233ab9d20c9e5960c61b61184543723
Tomas <tomasao87@gmail.com>
2023-02-15 20:24:11 -0300

    arreglo un error que no me dejaba usar la app

--- src/components/CartContainer/CartContainer.js
+++ src/components/CartContainer/CartContainer.js
@@ -1,7 +1,7 @@
 import { useContext } from "react"
 import { CartContext } from "../../context/CartContext"
 import { Link } from "react-router-dom"
-import { Center, Heading, Button } from "@chakra-ui/react"
+import { Center, Heading, Button, Text} from "@chakra-ui/react"
 
 const CartContainer = () =>{ 
     const {cart} = useContext(CartContext)
@@ -17,16 +17,14 @@ const CartContainer = () =>{
 
             {cart.map(prod =>{
                 return(
-                    <div>
-                        <Center>
-                            <div key={prod.id}>
-                                <h2>{prod.name}</h2>
+                        <Center key={prod.id}>
+                            <div >
+                                <Text>{prod.name}</Text>
                             </div>
+                            <Center>
+                                <Link to='/checkout'><Button>Checkout</Button></Link>
+                            </Center>
                         </Center>
-                        <Center>
-                            <Link to='/checkout'><Button>Checkout</Button></Link>
-                        </Center>
-                    </div>
                 )
             })}
             </div>
